cceba47f62f26076b41a432feefdd7bee74046fc
AmerSamer <73889178+AmerSamer@users.noreply.github.com>
2021-10-17 14:11:50 +0300

    3.1 EX

---
+++ src/components/3.1-boxes/Box1.js
@@ -0,0 +1,12 @@
+import Box2 from './Box2'
+import './style.css';
+
+const Box1 = () =>{
+ return (
+    <div className='box1'>
+        <Box2/>
+    </div>
+ )
+}
+
+export default Box1;
---
+++ src/components/3.1-boxes/Box4.js
@@ -0,0 +1,11 @@
+import './style.css';
+
+const Box4 = () =>{
+ return (
+    <div className='box4'>
+    Box4
+    </div>
+ )
+}
+
+export default Box4;
