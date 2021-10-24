323c4780f09cd0c3f27825bba860fe5328a46753
AmerSamer <73889178+AmerSamer@users.noreply.github.com>
2021-10-25 00:27:11 +0300

    13.2 ex

--- src/components/13.2-avatar/Avatar.js
+++ src/components/13.2-avatar/Avatar.js
@@ -1,70 +1,37 @@
-// import React from 'react';
-
-// const Avatar = (props) => {
-//     return (
-//         <div onClick={() => props.method(props.names)}>
-//             {props.names}
-//         </div>
-//     )
-// }
-
-// export default Avatar;
-
 import React from 'react';
-// import './style.css';
 
 class Avatar extends React.Component {
     constructor(props) {
         super(props);
         this.props = props;
-        this.state = {totalReactPackages: [] }
+        this.state = { totalReactPackages: [] }
     }
-    onc = async ()=>{
-        const response = await fetch('https://randomuser.me/api/');
-        const data = await response.json();
-        // console.log(data.results[0].picture.large);
-        // this.setState({ totalReactPackages: data.results[0].picture.large })
-    }
-    async componentDidMount(){
+
+    async componentDidMount() {
         let arr = [];
-        for(let i = 0 ; i < 10 ; i++){
+        for (let i = 0; i < 10; i++) {
             const response = await fetch('https://randomuser.me/api/');
             const data = await response.json();
-            // SHOUKD CHANGED
-            arr.push({ totalReactPackages: data.results[0].picture.large }) // SHOUKD CHANGED 
-            console.log(data);
+            arr.push({ totalReactPackages: data.results[0] })
         }
-        this.setState({ totalReactPackages:arr })
-    }
-    componentDidUpdate() {
-        // document.querySelector('.s').innerHTML = <img src={this.state.totalReactPackages} />
+        this.setState({ totalReactPackages: arr })
+        console.log(arr);
     }
 
     render() {
-        console.log(this.state.totalReactPackages);
         return (
             <div >
-
                 {
-                    this.state.totalReactPackages.map((u)=>{
-                        // SHOUKD CHANGED
-                        return <img src={u.totalReactPackages}/>   // SHOUKD CHANGED
-                    })
-                }
-               {/* {
-                    data.map((i, key) => {
+                    this.state.totalReactPackages.map((u) => {
                         return (
-                            <Names method={this.handleClickName} names={i.name} key={key} />
+                            <div>
+                                <img src={u.totalReactPackages.picture.large} />
+                                <br />
+                                {u.totalReactPackages.name.first} {u.totalReactPackages.name.last}
+                            </div>
                         )
                     })
-
-                } */}
-                {/* <h1>Pick Random Joke:</h1>
-                <input onClick={()=>this.onc()} type='button' value='Random Joke' />
-                <h2>Joke:</h2> */}
-                <div className='s'>
-                <img src={this.state.totalReactPackages} />
-                </div>
+                }
             </div>
         )
     }
