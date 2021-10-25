9e40c18aa1850df04ed076d70e0bb9422fa71de6
AmerSamer <73889178+AmerSamer@users.noreply.github.com>
2021-10-25 14:59:28 +0300

    18.2 ex

--- src/components/18.2-simple_todo/Todo.js
+++ src/components/18.2-simple_todo/Todo.js
@@ -1,10 +1,35 @@
 import React from "react";
+import './style.css'
+
+let arr = [
+    { name: "CSS", completed: true },
+    { name: "JavaScript", completed: true },
+    { name: "Learn React", completed: false },
+    { name: "Learn mongoDB", completed: false },
+    { name: "Learn Node JS", completed: false },
+]
 
 const Todo = () => {
 
+    const [array, setArray] = React.useState(arr)
+
+    const handkeClick = (e) => {
+        let newArr = [...array]
+        newArr[e.target.id].completed = !newArr[e.target.id].completed;
+        setArray(newArr)
+    }
+
     return (
         <div>
-            aaa
+            {
+                array.map((u, key) => {
+                    return (
+                        <li id={key} className={u.completed ? 'true' : 'false'} onClick={handkeClick} key={key}>
+                            {u.name}
+                        </li>
+                    )
+                })
+            }
         </div>
     )
 }
