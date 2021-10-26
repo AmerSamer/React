ebfe45ce8fca46d9a1ce7763457e16a1b7efc609
AmerSamer <73889178+AmerSamer@users.noreply.github.com>
2021-10-26 09:17:14 +0300

    19.2 ex

--- src/components/19.2-countries/CountriesWorld.jsx
+++ src/components/19.2-countries/CountriesWorld.jsx
@@ -2,20 +2,27 @@ import React from "react";
 
 const CountriesWorld = () => {
     const [items, setItems] = React.useState([])
+    const [fillItems, setFillItems] = React.useState([])
 
     React.useEffect(() => {
         fetch(`https://restcountries.com/v2/all`)
             .then(res => res.json())
-            .then(json => setItems(json))
+            .then(json => setItems(json))  
     }, [])
+
+    React.useEffect(() => {
+        setFillItems(items)
+    }, [items])
+
     const searchHandler = (e) => {
         const filterItems = items.filter(t => t.name.toLowerCase().indexOf(e.target.value) > -1);
-        setItems(filterItems)
+        setFillItems(filterItems)
     }
+    
     return (
         <div>
             <input type="search" name='search' htmlFor='search' onChange={searchHandler} />
-            {items.map((i,key) => {
+            {fillItems.map((i,key) => {
                 return <pre key={key}><li>{JSON.stringify(i.name)}</li></pre>
             })}
         </div>
